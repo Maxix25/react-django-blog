8f69a71ea696ad68db39277db38aa26dfd5f0fb3
maxi <maximiliano.besoain@gmail.com>
2025-03-11 19:39:04 -0300

    Set up Docker for backend and frontend, add blog app with REST API

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(status=status.HTTP_200_OK, data=serializer.data)
