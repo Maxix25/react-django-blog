9885b3438167ab226a57bd0eeaebe1c74d8d9050
Maxi <maximiliano.besoain@gmail.com>
2025-07-19 01:04:36 -0400

    Refactor API initialization to conditionally set baseURL based on environment

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
+let api;
+
+if (import.meta.env.PROD) {
+    api = axios.create({
+        baseURL: 'https://react-django-blog-0axx.onrender.com',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+} else {
+    api = axios.create({
+        baseURL: 'http://127.0.0.1:8000',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
 
 axios.interceptors.request.use(
     (config) => {
